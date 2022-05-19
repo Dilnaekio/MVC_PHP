92e05dc894eb90d95ff8eed2a94b9f91035ea11b
Delcourt <delcourtfabian@gmail.com>
2022-05-19 16:32:18 +0200

    Petite modif visuelle pour l'accueil

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,6 +1,11 @@
 <?php ob_start() ?>
 
-<p>AccueilView.php</p>
+<main class="container">
+    <section>
+        <p>KOUKOU, il n'y a rien sur cette page, go cliquer sur "livres"</p>
+    </section>
+
+</main>
 
 <?php $title = "Accueil";
 $content = ob_get_clean();
